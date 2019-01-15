33d0b1adb1e5aa5c33b28c32157800ee8b35f8a9
Talgat <t2002t@mail.ru>
2019-01-15 14:34:02 +0600

    Это наш первый комит

---
+++ lessons/script.js
@@ -0,0 +1,21 @@
+
+console.log('Something');
+console.log(9*8);
+
+let persone = {
+    name:'John',
+    age:25,
+    isMarried: false
+};
+
+console.log(persone.name);
+//
+//alert('Hello World!');
+
+//let answer = confirm('Are you here?');
+//console.log(answer);
+
+
+let answer = prompt('Есть ли Вам 18 лет', "Да");
+console.log(answer);
+console.log(typeof(answer));
